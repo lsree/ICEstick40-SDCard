2ecaeeeafe3b16c005d0559689b659d16a8522df
lsree <locchanan@gmail.com>
2023-04-21 10:13:51 -0400

    Testbench works. Need to redesign SPI slave state machine

--- SPI_Blinky/SPI_Blinky/spi_slave.v
+++ SPI_Blinky/SPI_Blinky/spi_slave.v
@@ -1,15 +1,21 @@
+//TODO
+//Need to redesign and make a deliberate statemachine
+//Need to consider multi-byte and single byte transmissions
+//Need to consider how long SCK clocks as part of that consideration
+
+
 module spi_slave(
     input i_clk,        //Sys clk   
     input i_sys_rst,    //sys reset
     input i_sck,        //SPI SCK
     input i_MOSI,       //Master Out Slave In
     input i_cs,         //Active low
-    output o_MISO,      //Master In Slave Out
-    output reg [7:0] o_rx_byte; //Stored received byte
-    output reg o_rx_rdy;        //Notifies that byte is ready to be read
-    input [7:0] i_tx_byte;      //Byte to send  
-    input i_tx_rdy;             //Notifies that transmission is ready
-)
+    output reg o_MISO,      //Master In Slave Out
+    output reg [7:0] o_rx_byte, //Stored received byte
+    output reg o_rx_rdy,        //Notifies that byte is ready to be read
+    input [7:0] i_tx_byte,      //Byte to send  
+    input i_tx_rdy             //Notifies that transmission is ready
+);
     //Assumes CPOL=0, CPHA=0
 
 
@@ -27,25 +33,26 @@ module spi_slave(
             r_rx_sck_full_byte <= 0;
         end
         else begin 
-            rx_cntr <= rx_cntr + 3'd1;
-            rx_byte_r <= {i_MOSI, rx_byte_r[7:1]};    //Rx and store MSB
-            if (rx_cntr == 3'b111)                  //Received 8 bits
+            r_rx_cntr <= r_rx_cntr + 3'd1;
+            r_rx_byte <= {i_MOSI, r_rx_byte[7:1]};    //Rx and store MSB
+            if (r_rx_cntr == 3'b111) begin                  //Received 8 bits
                 r_sck_rx_rdy <= 1'b1;
-                r_rx_sck_full_byte <= rx_byte_r;  //Store completed byte
-            else if (rx_cntr == 3'b010)     //r_sck_rx_rdy will be asserted for 2x sck cycles. Hopefully this is enough
+                r_rx_sck_full_byte <= r_rx_byte;  //Store completed byte
+            end
+            else if (r_rx_cntr == 3'b010)     //r_sck_rx_rdy will be asserted for 2x sck cycles. Hopefully this is enough
                 r_sck_rx_rdy <= 1'b0;
         end
     end 
 
     //Handle clock domain crossing from SPI SCK -> SYS CLK
     reg [1:0] r_rx_rdy_r_edge;    //Used to detect when rx byte is rdy. 0 pos is current value, 1 pos is last value
-    always @ (posedge clk) begin
-        if (~sys_rst) begin
-            rx_rdy_r_edge <= 2'd0;
+    always @ (posedge i_clk) begin
+        if (~i_sys_rst) begin
+            r_rx_rdy_r_edge <= 2'd0;
         end
         else begin
-            rx_rdy_r_edge <= {rx_rdy_r_edge[0], rx_rdy_r};
-            if (rx_rdy_r_edge == 2'b01) begin       //Rising edge
+            r_rx_rdy_r_edge <= {r_rx_rdy_r_edge[0], r_sck_rx_rdy};
+            if (r_rx_rdy_r_edge == 2'b01) begin       //Rising edge
                o_rx_rdy <= 1'b0;
                o_rx_byte <= r_rx_sck_full_byte;
             end
@@ -56,7 +63,7 @@ module spi_slave(
 
     //TX Bytes
     reg [7:0] r_tx_byte;    //Tx byte is buffered so parent modules can modify input
-    always @ (posedge clk) begin
+    always @ (posedge i_clk) begin
         if (~i_sys_rst)  
            r_tx_byte <= 8'h0; 
         else begin
--- SPI_Blinky/spi_slave_tb.v
+++ SPI_Blinky/spi_slave_tb.v
@@ -7,11 +7,13 @@ parameter SCK_DELAY = 3;
 
 reg r_clk;
 reg r_sck;
+reg r_rst;
 
 reg r_MOSI, r_tx_rdy;
 reg r_cs;
 wire w_MISO, w_rx_rdy;
-wire [7:0] w_rx_byte, w_tx_byte;
+wire [7:0] w_rx_byte;
+reg [7:0] r_tx_byte;
 
 
 spi_slave slave1(
@@ -22,37 +24,38 @@ spi_slave slave1(
     //SPI Signals 
     .i_sck(r_sck), 
     .i_MOSI(r_MOSI),
-    .i_cs(r_cs) 
-    .o_MISO(w_MISO)
+    .i_cs(r_cs),
+    .o_MISO(w_MISO),
 
     //Data Signals
-    .o_rx_byte(w_rx_byte)
-    .o_rx_rdy(w_rx_rdy)
-    .i_tx_byte(w_tx_byte)
+    .o_rx_byte(w_rx_byte),
+    .o_rx_rdy(w_rx_rdy),
+    .i_tx_byte(r_tx_byte),
     .i_tx_rdy(r_tx_rdy)
 );
-reg [7:0] r_master_rx_data;
+wire [7:0] r_master_rx_data;
 SPI_RX_Master spi_master1(
-    .i_sck(r_sck)
+    .i_sck(r_sck),
     .i_cs(r_cs), 
     .i_MISO(w_MISO), 
-    .reg o_rx_data(r_master_rx_data);
-)
+    .o_rx_data(r_master_rx_data)
+);
 
 integer i;
 task sendSingleByte(input [7:0]data);
     begin
         r_cs = 0;
-
+        #1;
         for (i = 0; i < 8; i = i + 1)
         begin 
-            #(SCK_DELAY)
+            #(SCK_DELAY);
             r_sck = !r_sck;
             r_MOSI = data[i];
-            #(SCK_DELAY)
+            #(SCK_DELAY);
             r_sck = !r_sck;
         end
-       r_cs = 1; 
+        #1;
+        r_cs = 1; 
     end
 endtask
 
@@ -60,7 +63,7 @@ task loadByte(input [7:0] data);
 begin
     @(posedge r_clk)
     begin
-        w_tx_byte = data;
+        r_tx_byte = data;
         r_tx_rdy = 1;
     end
     @(posedge r_clk)
@@ -75,10 +78,15 @@ reg [7:0] test_bytes;
 initial
     begin
     	r_clk = 0;
-    	r_cs = 1;
+        //Hold under reset
+     	r_cs = 1;
     	r_MOSI = 0;
         r_sck = 1;
-        //Simulate Receiving bytes from 0-255
+        r_rst = 0;
+        #10;
+
+        r_rst = 1;
+       //Simulate Receiving bytes from 0-255
         for(test_bytes = 0; test_bytes < 8'hFF; test_bytes = test_bytes + 1)
         begin
             sendSingleByte(test_bytes);
@@ -86,10 +94,10 @@ initial
             begin
                 //Check for notification
                 if (w_rx_rdy != 0)
-                    $display("Failed to notify that byte was received")
+                    $display("Failed to notify that byte was received");
                 //Check for correct value
                 if (test_bytes != w_rx_byte)
-                    $display("Failed receive correct value")
+                    $display("Failed receive correct value");
             end
             #10;
         end
@@ -101,7 +109,7 @@ initial
             repeat(8) @(posedge r_sck);     //Wait for byte to get clocked out
             //Check if value was sent correctly
             if (test_bytes != r_master_rx_data)
-                $display("Failed to properly send data")
+                $display("Failed to properly send data");
             #10;
         end
 
@@ -114,14 +122,17 @@ module SPI_RX_Master (
     input i_sck, 
     input i_cs, 
     input i_MISO, 
-    output reg o_rx_data[7:0];
-)
+    output [7:0] o_rx_data
+);
 
+    reg [7:0] r_rx_data;
     always @ (posedge i_sck or posedge i_cs) begin
         if (i_cs)
-            o_rx_data <=0;
+            r_rx_data <=0;
         else begin
-            o_rx_data <= {i_MISO, o_rx_data[7:1]};
+            r_rx_data <= {i_MISO, r_rx_data[7:1]};
         end
     end
+
+    assign o_rx_data = r_rx_data;
 endmodule
